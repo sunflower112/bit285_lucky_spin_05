3d3b98c5aeaf79f220719bd49503e67c48414d0a
sunskhine112 <cherrybomb411@gmail.com>
2020-01-23 15:11:27 -0800

    * SpinnerController.cs: Yeah it works so far
    * Player.cs:
    * Index.cshtml:

--- LuckySpin/Models/Player.cs
+++ LuckySpin/Models/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System;
 //TODO: Add the library for Data Annotations
 namespace LuckySpin.Models
 {
@@ -7,9 +8,13 @@ namespace LuckySpin.Models
         //TODO: Include two annotations for FirstName
         // one that makes it Required (no empty strings)
         // and another that sets its Display prompt to "Your First Name"
+        [Required()]
+        [Display(Prompt = "Please enter your name")]
         public string FirstName { get; set; }
         //TODO: Include one annotation for LuckyNumber
         // one that limits its Range between 1 and 9
+        [Range(1, 9, ErrorMessage ="Choose a number")]
+  
         public int LuckyNumber { get; set; }
     }
 }
